ae96f976b5d5a83d59aa16f65a1e3f18f4eaa55b
marikuri94 <daydie94@mail.ru>
2022-05-02 17:29:54 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+
+// let message = 'Hello!';
+// let message2;
+// message2 = 'world'; 
+
+
+// let x = 5;
+// let y = x;
+// x = 3;
+
+// let user = 'Dacha';
+// let age = 26;
+// let number = '9022369706';
+// console.log(number);
+
+// let message_to = `Hello, ${user}`;
+// // alert (message_to);
+
+
+// let yes = true;
+// let no = false;
+
+// let a;
+// alert (a);
+
+// let b = null; 
+
+// let name = "Ilya";
+// alert (`hello ${1}`);
+// alert (`hello ${"name"}`);
+// alert (`hello ${name}`);
+
+// var x = 5;         
+// var y = 2;        
+// var z = x + y;
+// alert(z);
+
+// let a = " \t \n" - 2 ;
+// alert (a);
+// "" + 1 + 0
+// "" - 1 + 0
+// true + false
+// 6 / "3"
+// "2" * "3"
+// 4 + 5 + "px"
+// "$" + 4 + 5
+// "4" - 2
+// "4px" - 2
+// 7 / 0
+// "  -9  " + 5
+// "  -9  " - 5
+// null + 1
+// undefined + 1
+// " \t \n" - 2 
+
+// let a = +prompt("Первое число?", 1);
+// let b = +prompt("Второе число?", 2);
+
+// alert(a + b);
+
+
+// решение домашки 1. 
+// var button = document.querySelector('button');
+// button.onclick = function() {
+// var name = prompt('What is your name?');
+// alert('Hello ' + name + ', nice to see you!');
+// } 
+
+// function sum(x, y=0) {
+//     return x+y;
+// } 
+// alert(sum(5));
+
+// решение домашки 1
+var button = document.querySelector('button');
+button.onclick = function() {
+var name = prompt('What is your name?');
+alert('Hello ' + name + ', welcome to nice site!');
+} 
